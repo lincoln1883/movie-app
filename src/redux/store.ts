eb3d49af28d598ed147d4c41a92f2fc88ab6a2f1
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-23 07:49:20 -0300

    Configure the store.

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+
+
+const store = configureStore({
+	reducer: {
+		// Here we will be adding reducers
+	},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
