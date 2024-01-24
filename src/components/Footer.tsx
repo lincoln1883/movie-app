49c4f0df2f0f86c231590d9211789805f9a246e7
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-24 17:19:02 -0300

    Add footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className="flex justify-center items-center h-16 bg-black text-white">
+    <p className="text-center text-sm sm:text-base">
+      &copy; 2023 by Lincoln W Gibson All Rights Reserved.
+    </p>
+  </footer>
+);
+
+export default Footer;
