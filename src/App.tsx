8cc408e16cbe82a50ebc334e7afba6c18b0e7aa9
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-24 17:18:19 -0300

    Update the app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import MovieList from './features/movies/MovieList'
-import MovieDetails from './features/movies/MovieDetails'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import MovieList from './features/movies/MovieList';
+import MovieDetails from './features/movies/MovieDetails';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MovieList />} />
-      <Route path="/movies/:id" element={<MovieDetails />} />
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<MovieList />} />
+          <Route path="/movies/:id" element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
