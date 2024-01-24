bd062f65b5e19a23ce15293a66ea81ec73b09e88
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-24 17:14:24 -0300

    Update the movie details component

--- src/features/movies/MovieDetails.tsx
+++ src/features/movies/MovieDetails.tsx
@@ -2,6 +2,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { fetchMovieById } from './movieSlice';
 import { useEffect } from 'react';
+import { Spinner } from 'flowbite-react';
 
 const MovieDetails = () => {
   const { id } = useParams();
@@ -9,16 +10,11 @@ const MovieDetails = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log('useEffect', id);
     dispatch(fetchMovieById(id as string));
   }, [dispatch, id]);
 
   const loading = useAppSelector((state) => state.movies.status === 'loading');
   const movies = useAppSelector((state) => state.movies.movie);
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
 
   if (!movies) {
     return <h1>No movie found</h1>;
@@ -29,26 +25,32 @@ const MovieDetails = () => {
   };
 
   return (
-    <>
+    <div className='mx-20'>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={goBack}
       >
         Go Back
       </button>
-      <div className="flex flex-col justify-center items-center m-auto w-2/4">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-3xl font-bold">{movies.title}</h1>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
-            alt={movies.title}
-            className="w-1/2"
-          />
-          <p className="text-lg">{movies.overview}</p>
-          <p className="text-lg">{movies.vote_average}</p>
+          {loading && <Spinner aria-label="Default status example" />}
+      <div className="flex h-screen w-100">
+        <div className="flex flex-col justify-center items-center w-100 sm:flex-row">
+          <div className='flex items-center justify-center w-full h-full flex-1'>
+            <img
+                src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
+                alt={movies.title}
+                className="w-full h-3/4 rounded-lg shadow-md"
+                />
+          </div>
+          <div className='flex flex-col justify-center items-start flex-1 px-3'>
+            <h3 className="text-lg px-1"><span className='text-2xl font-bold'>Title: </span>{movies.title}</h3>
+            <p className="text-lg px-1"><span className='text-2xl font-bold'>Details: </span>{movies.overview}</p>
+            <p className="text-lg px-1"><span className='text-2xl font-bold'>Rating: </span>{movies.vote_average}</p>
+            <p className='text-lg px-1'><span className='text-2xl font-bold'>Released: </span>{movies.release_date}</p>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
