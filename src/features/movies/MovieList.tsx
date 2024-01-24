c74dc03b70dc4d24b92d93843dd33e5c8a667975
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-24 17:13:13 -0300

    Update movie list component.

--- src/features/movies/MovieList.tsx
+++ src/features/movies/MovieList.tsx
@@ -2,10 +2,13 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../redux/store";
 import Movie from "./Movie";
 import { fetchMovies } from "./movieSlice";
+import MovieSearch from "./MovieSearch";
+import { Spinner } from 'flowbite-react';
 
 export default function MovieList() {
 	const dispatch = useAppDispatch();
 	const movies = useAppSelector((state) => state.movies.movies);
+	const loading = useAppSelector((state) => state.movies.status === 'loading');
 
 	useEffect(() => {
 		dispatch(fetchMovies());
@@ -13,15 +16,16 @@ export default function MovieList() {
 
 	return (
 		<>
-		<h1 className="text-center text-lg">Popular Movies</h1>
-    <div className="flex flex-col sm:flex-row justify-center items-center">
-      <ul className="grid grid-cols-2 gap-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 justify-center sm:gap-3 px-3 pr-3 mb-8">
+		{loading && <Spinner aria-label="Default status example" />}
+		<MovieSearch />
+    <div className="flex justify-center items-center">
+      <ul className="grid grid-cols-2 gap-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 justify-center sm:gap-2 px-3 pr-3 mb-8">
         {movies.map((movie) => (
           <Movie
 					key={movie.id}
 					movie={movie}
           />
-        ))}
+					))}
       </ul>
     </div>
 		</>
