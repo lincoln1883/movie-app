8d35d26da2c2d4fc1274e24bdb0a49135d209fe6
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-25 11:56:48 -0300

    Add lint and format.

--- src/features/movies/MovieDetails.tsx
+++ src/features/movies/MovieDetails.tsx
@@ -25,30 +25,45 @@ const MovieDetails = () => {
   };
 
   return (
-    <div className='mx-20'>
+    <div className="mx-20">
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={goBack}
       >
         Go Back
       </button>
-          {loading && <Spinner aria-label="Default status example" />}
       <div className="flex h-screen w-100">
-        <div className="flex flex-col justify-center items-center w-100 sm:flex-row">
-          <div className='flex items-center justify-center w-full h-full flex-1'>
-            <img
+        {loading ? (
+          <Spinner aria-label="Default status example" />
+        ) : (
+          <div className="flex flex-col justify-center items-center w-100 sm:flex-row">
+            <div className="flex items-center justify-center w-full h-full flex-1">
+              <img
                 src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
                 alt={movies.title}
                 className="w-full h-3/4 rounded-lg shadow-md"
-                />
+              />
+            </div>
+            <div className="flex flex-col justify-center gap-1 items-start flex-1 px-3">
+              <h3 className="text-lg px-1">
+                <span className="text-2xl font-bold">Title: </span>
+                {movies.title}
+              </h3>
+              <p className="text-lg px-1">
+                <span className="text-2xl font-bold">Details: </span>
+                {movies.overview}
+              </p>
+              <p className="text-lg px-1">
+                <span className="text-2xl font-bold">Rating: </span>
+                {movies.vote_average}
+              </p>
+              <p className="text-lg px-1">
+                <span className="text-2xl font-bold">Released: </span>
+                {movies.release_date}
+              </p>
+            </div>
           </div>
-          <div className='flex flex-col justify-center items-start flex-1 px-3'>
-            <h3 className="text-lg px-1"><span className='text-2xl font-bold'>Title: </span>{movies.title}</h3>
-            <p className="text-lg px-1"><span className='text-2xl font-bold'>Details: </span>{movies.overview}</p>
-            <p className="text-lg px-1"><span className='text-2xl font-bold'>Rating: </span>{movies.vote_average}</p>
-            <p className='text-lg px-1'><span className='text-2xl font-bold'>Released: </span>{movies.release_date}</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- src/features/movies/MovieList.tsx
+++ src/features/movies/MovieList.tsx
@@ -1,33 +1,33 @@
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../redux/store";
-import Movie from "./Movie";
-import { fetchMovies } from "./movieSlice";
-import MovieSearch from "./MovieSearch";
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
+import Movie from './Movie';
+import { fetchMovies } from './movieSlice';
+import MovieSearch from './MovieSearch';
 import { Spinner } from 'flowbite-react';
 
 export default function MovieList() {
-	const dispatch = useAppDispatch();
-	const movies = useAppSelector((state) => state.movies.movies);
-	const loading = useAppSelector((state) => state.movies.status === 'loading');
+  const dispatch = useAppDispatch();
+  const movies = useAppSelector((state) => state.movies.movies);
+  const loading = useAppSelector((state) => state.movies.status === 'loading');
 
-	useEffect(() => {
-		dispatch(fetchMovies());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
 
-	return (
-		<>
-		{loading && <Spinner aria-label="Default status example" />}
-		<MovieSearch />
-    <div className="flex justify-center items-center">
-      <ul className="grid grid-cols-2 gap-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 justify-center sm:gap-2 px-3 pr-3 mb-8">
-        {movies.map((movie) => (
-          <Movie
-					key={movie.id}
-					movie={movie}
-          />
-					))}
-      </ul>
-    </div>
-		</>
+  return (
+    <>
+      <MovieSearch />
+      {loading ? (
+        <Spinner aria-label="Default status example" />
+      ) : (
+        <div className="flex justify-center items-center">
+          <ul className="grid grid-cols-2 gap-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 justify-center sm:gap-2 px-3 pr-3 mb-8">
+            {movies.map((movie) => (
+              <Movie key={movie.id} movie={movie} />
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
