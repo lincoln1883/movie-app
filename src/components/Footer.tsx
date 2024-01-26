c7b45e77f55300f82d67a39a1283dee0706e06c2
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-26 11:45:14 -0300

    Change the background color.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="flex justify-center items-center h-16 bg-black text-white">
+  <footer className="flex justify-center items-center h-16 bg-blue-200 -mb-0">
     <p className="text-center text-sm sm:text-base">
       &copy; 2023 by Lincoln W Gibson All Rights Reserved.
     </p>
