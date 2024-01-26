b028ecd84bf3ce2c7d4b6968b380ce9bd630c439
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-26 11:45:59 -0300

    Add home component to display the jumbotron.

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import Jumbotron from './Jumbotron';
+
+const Home = () => {
+	return (
+    <div className="relative">
+      <div className="w-full h-full object-cover">
+        <Jumbotron />
+      </div>
+    </div>
+  );
+}
+
+export default Home
