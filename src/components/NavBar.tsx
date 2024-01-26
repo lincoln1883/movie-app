9553f36c26314ab47cdd4628dcae74c337448190
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-26 11:48:55 -0300

    Fix navbar functionality and layout.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,35 +1,23 @@
-import { useLocation, Link } from 'react-router-dom';
-import Movie from '../features/movies/Movie';
 import { Navbar } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  const location = useLocation();
-
-  const title = (id: string) => {
-    if (location.pathname === '/') {
-      return <p>Popular Movies</p>;
-    }
-    if (location.pathname === `/movies/${id}`) {
-      return <p>Movie Details</p>;
-    }
-    return <p>Movie Details</p>;
-  };
-
   return (
-    <Navbar fluid rounded className='bg-blue-200 h-16'>
-      <h1 className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+    <Navbar fluid rounded className='bg-blue-200 h-30 py-5'>
+      <h1 className="sm:self-center whitespace-nowrap text-xl font-semibold dark:text-white">
         Movie App
       </h1>
-      <div className="flex justify-center">
-        <h1 className="text-2xl">{title(Movie.name)}</h1>
-      </div>
+
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href='#' active>
+        <Navbar.Link as={Link} to='/' active>
           Home
         </Navbar.Link>
-        <Navbar.Link as={Link} href="#">
-          About
+        <Navbar.Link as={Link} to='/movies'>
+          Movies
+        </Navbar.Link>
+        <Navbar.Link as={Link} to='/shows'>
+          Shows
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
