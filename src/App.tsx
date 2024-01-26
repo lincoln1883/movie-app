2b35056927811afef998d9c70f99645fe0bc4f3d
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-26 11:44:44 -0300

    Add additional components to display.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import MovieList from './features/movies/MovieList';
 import MovieDetails from './features/movies/MovieDetails';
 import Layout from './components/Layout';
+import Home from './components/Home';
+import ShowDetails from './features/shows/ShowDetails';
+import ShowList from './features/shows/ShowList';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} >
-          <Route index element={<MovieList />} />
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<MovieList />} />
           <Route path="/movies/:id" element={<MovieDetails />} />
+          <Route path="/shows" element={<ShowList />} />
+          <Route path="/shows/:id" element={<ShowDetails />} />
         </Route>
       </Routes>
     </>
