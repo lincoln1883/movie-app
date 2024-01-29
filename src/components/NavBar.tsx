5d98e3db242dd1dd1ad579bcc5a76b4cd9c3bbf8
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-29 09:02:22 -0300

    Change title.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <Navbar fluid rounded className='bg-blue-200 h-30 py-5'>
-      <h1 className="sm:self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        Movie App
+      <h1 className="sm:self-center whitespace-nowrap text-xl font-semibold dark:text-white capitalize">
+        pipoca filmes
       </h1>
-
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Navbar.Link as={Link} to='/' active>
