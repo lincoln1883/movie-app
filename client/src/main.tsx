022ee23f3d2a8c196c5a5def07590ffd7719e367
Lincoln Gibson <lincolngibson7@gmail.com>
2024-02-09 12:15:25 -0300

    breaking charge:
    add dev branch to create a full-stack app.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./redux/store.ts";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
