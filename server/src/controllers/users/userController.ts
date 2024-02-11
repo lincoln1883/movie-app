2ff0fef8219150371f07a04b63a63ac786cbbd2c
Lincoln Gibson <lincolngibson7@gmail.com>
2024-02-11 09:55:56 -0300

    Refactor the create user function to improver error handling.

--- server/src/controllers/users/userController.ts
+++ server/src/controllers/users/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import User from "../../models/User";
+import { omitPassword,verifyPassword } from "../../utils/passwordHelper";
 
 const createUser = async (req: Request, res: Response) => {
 	try {
@@ -15,10 +16,22 @@ const createUser = async (req: Request, res: Response) => {
 		) {
 			return res.status(400).json({ error: "All fields are required" });
 		}
+		if (email.indexOf("@") === -1 || email.indexOf(".") === -1) {
+			return res.status(400).json({ error: "Invalid email" });
+		}
+		if (!verifyPassword(password)) {
+			return res
+				.status(400)
+				.json({ error: "Password must be at least 6 characters uppercase, lowercase and digit." });
+		}
+		const existingUser = await User.findOne({ email });
+		if (existingUser) {
+			return res.status(409).json({ error: "User already exists" });
+		}
 		const hashedPassword = await bcrypt.hashSync(password, 10);
 		const user = new User({ username, email, password: hashedPassword });
 		await user.save();
-		res.json(user);
+		return res.status(201).json(omitPassword(user.toObject()));
 	} catch (error: unknown | any) {
 		res.status(500).json({ error: error.message });
 		throw error as Error;
