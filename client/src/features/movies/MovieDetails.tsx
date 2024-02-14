1d34fa3baa8ba49c2077a02056486292c1ee48f8
Lincoln Gibson <lincolngibson7@gmail.com>
2024-02-14 11:33:13 -0300

    Add feature for user to comment on item.

--- client/src/features/movies/MovieDetails.tsx
+++ client/src/features/movies/MovieDetails.tsx
@@ -5,82 +5,109 @@ import { useEffect } from "react";
 import MovieModal from "./MovieModal";
 import { Rating, Spinner } from "flowbite-react";
 import MovieCredit from "../credits/movieCredits/MovieCredit";
+import { FaBackspace } from "react-icons/fa";
 
 const MovieDetails = () => {
-  const { id } = useParams();
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
+	const { id } = useParams();
+	const dispatch = useAppDispatch();
+	const Navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(fetchMovieById(id as string));
-  }, [dispatch, id]);
+	useEffect(() => {
+		dispatch(fetchMovieById(id as string));
+	}, [dispatch, id]);
 
-  const loading = useAppSelector((state) => state.movies.status === "loading");
-  const movies = useAppSelector((state) => state.movies.movie);
+	const loading = useAppSelector((state) => state.movies.status === "loading");
+	const movies = useAppSelector((state) => state.movies.movie);
 
-  if (!movies) {
-    return <h1>No movie found</h1>;
-  }
+	if (!movies) {
+		return <h1>No movie found</h1>;
+	}
 
-  const goBack = () => {
-    navigate(-1);
-  };
+	const goBack = () => {
+		Navigate(-1);
+	};
 
-  const ratingOutOf5 = movies.vote_average / 2;
-  const numberOfStars = Math.round(ratingOutOf5);
+	const ratingOutOf5 = movies.vote_average / 2;
+	const numberOfStars = Math.round(ratingOutOf5);
 
-  const starComponents = Array.from({ length: numberOfStars }, (_, index) => (
-    <Rating.Star key={index} />
-  ));
+	const starComponents = Array.from({ length: numberOfStars }, (_, index) => (
+		<Rating.Star key={index} />
+	));
 
-  return (
-    <div className="m-5 sm:mx-12 mt-5">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={goBack}
-      >
-        Go Back
-      </button>
-      <div className="flex min-h-screen w-100">
-        {loading ? (
-          <Spinner aria-label="Default status example" />
-        ) : (
-          <div className="flex flex-col justify-center items-center w-100 sm:flex-row">
-            <div className="flex items-center justify-center w-full h-3/4 sm:w-full sm:h-full flex-1">
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
-                alt={movies.title}
-                className="w-3/4 h-3/4 sm:w-full sm:h-3/4 rounded-lg shadow-md"
-              />
-            </div>
-            <div className="flex flex-col justify-center gap-1 items-start flex-1 px-3">
-              <h3 className="text-lg px-1">
-                <span className="text-lg font-bold">Title: </span>
-                {movies.title}
-              </h3>
-              <p className="line-clamp-4 sm:text-lg px-1">
-                <span className="text-lg font-bold">Details: </span>
-                {movies.overview}
-              </p>
-              <div className="flex">
-                <p className="text-lg px-1 w-full">
-                  <span className="text-lg font-bold">Rating: </span>
-                </p>
-                <Rating>{starComponents}</Rating>
-              </div>
-              <p className="text-lg px-1">
-                <span className="text-lg font-bold">Released: </span>
-                {movies.release_date}
-              </p>
-              <MovieModal movies={movies} />
-            </div>
-          </div>
-        )}
-      </div>
-      <hr className="border m-4" />
-      <MovieCredit />
-    </div>
-  );
+	return (
+		<div className="m-5 sm:mx-12 mt-5">
+			<div className="h-10 flex shadow-md">
+				<FaBackspace
+					className="text-blue-500 text-2xl mx-1 self-center"
+					onClick={goBack}
+				/>
+				<span className="self-center">Go Back</span>
+			</div>
+			<div className="flex min-h-screen w-100">
+				{loading ? (
+					<Spinner aria-label="Default status example" />
+				) : (
+					<div className="flex flex-col justify-center items-center w-100 sm:flex-row shadow-md p-3">
+						<div className="flex items-center justify-center w-full h-3/4 sm:w-full sm:h-full flex-1 m-1">
+							<img
+								src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
+								alt={movies.title}
+								className="w-full h-full sm:w-full sm:h-3/4 rounded-lg"
+							/>
+						</div>
+						<div className="flex flex-col w-full sm:w-1/2 gap-2">
+							<div className="flex flex-col justify-center gap-1 items-start flex-1 px-3">
+								<h3 className="text-lg px-1">
+									<span className="text-lg font-bold">Title: </span>
+									{movies.title}
+								</h3>
+								<p className="line-clamp-4 sm:text-lg px-1">
+									<span className="text-lg font-bold">Details: </span>
+									{movies.overview}
+								</p>
+								<div className="flex">
+									<p className="text-lg px-1 w-full">
+										<span className="text-lg font-bold">Rating: </span>
+									</p>
+									<Rating>{starComponents}</Rating>
+								</div>
+								<p className="text-lg px-1">
+									<span className="text-lg font-bold">Released: </span>
+									{movies.release_date}
+								</p>
+								<div className="flex justify-center items-center">
+									<MovieModal movies={movies} />
+								</div>
+							</div>
+							<div className="flex flex-col justify-center gap-1 items-start flex-1 px-3">
+								<div className="flex flex-col gap-1 w-full">
+									<form className="w-full">
+										<label htmlFor="review">Review:</label>
+										<textarea
+											name="review"
+											id="review"
+											placeholder="Write a review..."
+											className="w-full h-40 p-2 rounded-lg shadow-md"
+										></textarea>
+										<button
+											type="submit"
+											className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+										>
+											Submit
+										</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+			<hr className="border m-4" />
+			<div className="shadow-md p-2">
+				<MovieCredit />
+			</div>
+		</div>
+	);
 };
 
 export default MovieDetails;
--- client/src/features/shows/ShowDetails.tsx
+++ client/src/features/shows/ShowDetails.tsx
@@ -5,82 +5,110 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Spinner, Rating } from "flowbite-react";
 import ShowModal from "./ShowModal";
 import ShowCredit from "../credits/showCredits/ShowCredit";
+import { FaBackspace, FaThumbsUp } from "react-icons/fa";
 
 const ShowDetails = () => {
-  const { id } = useParams();
-  const Navigate = useNavigate();
+	const { id } = useParams();
+	const Navigate = useNavigate();
 
-  const dispatch = useAppDispatch();
-  const show = useAppSelector((state) => state.shows.show);
-  const loading = useAppSelector((state) => state.shows.status === "loading");
+	const dispatch = useAppDispatch();
+	const show = useAppSelector((state) => state.shows.show);
+	const loading = useAppSelector((state) => state.shows.status === "loading");
 
-  useEffect(() => {
-    dispatch(fetchShowById(id as string));
-  }, [dispatch, id]);
+	useEffect(() => {
+		dispatch(fetchShowById(id as string));
+	}, [dispatch, id]);
 
-  if (!show) {
-    return <h1>Not found</h1>;
-  }
+	if (!show) {
+		return <h1>No show found</h1>;
+	}
 
-  const goBack = () => {
-    Navigate(-1);
-  };
+	const goBack = () => {
+		Navigate(-1);
+	};
 
-  const ratingOutOf5 = show.vote_average / 2;
-  const numberOfStars = Math.round(ratingOutOf5);
+	const ratingOutOf5 = show.vote_average / 2;
+	const numberOfStars = Math.round(ratingOutOf5);
 
-  const starComponents = Array.from({ length: numberOfStars }, (_, index) => (
-    <Rating.Star key={index} />
-  ));
+	const starComponents = Array.from({ length: numberOfStars }, (_, index) => (
+		<Rating.Star key={index} />
+	));
 
-  return (
-    <div className="m-5 sm:mx-12 mt-5">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={goBack}
-      >
-        Go Back
-      </button>
-      <div className="flex h-screen w-100">
-        {loading ? (
-          <Spinner aria-label="Default status example" />
-        ) : (
-          <div className="flex flex-col justify-center items-center w-100 sm:flex-row">
-            <div className="flex items-center justify-center w-full h-3/4 sm:w-full sm:h-full flex-1">
-              <img
-                src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}
-                alt={show.name}
-                className="w-3/4 h-3/4 sm:w-full sm:h-3/4 rounded-lg shadow-md"
-              />
-            </div>
-            <div className="flex flex-col justify-center gap-1 items-start flex-1 px-3">
-              <h3 className="text-lg px-1">
-                <span className="text-lg font-bold">Title: </span>
-                {show.name}
-              </h3>
-              <p className="line-clamp-4 sm:text-lg px-1">
-                <span className="text-lg font-bold">Details: </span>
-                {show.overview}
-              </p>
-              <div className="flex">
-                <p className="text-lg px-1 w-full">
-                  <span className="text-lg font-bold">Rating: </span>
-                </p>
-                <Rating>{starComponents}</Rating>
-              </div>
-              <p className="text-lg px-1">
-                <span className="text-lg font-bold">First Air Date: </span>
-                {show.first_air_date}
-              </p>
-              <ShowModal show={show} />
-            </div>
-          </div>
-        )}
+	return (
+		<div className="m-5 sm:mx-12 mt-5">
+      <div className="h-10 flex shadow-md">
+        <FaBackspace
+          className="text-blue-500 text-2xl mx-1 self-center"
+          onClick={goBack}
+        />
+        <span className="self-center">Go Back</span>
       </div>
-      <hr className="border m-4" />
-      <ShowCredit />
-    </div>
-  );
+			<div className="flex min-h-screen w-100">
+				{loading ? (
+					<Spinner aria-label="Default status example" />
+				) : (
+					<div className="flex flex-col justify-center items-center w-100 sm:flex-row shadow-md p-3">
+						<div className="flex items-center justify-center w-full h-3/4 sm:w-full sm:h-full flex-1">
+							<img
+								src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}
+								alt={show.name}
+								className="w-full h-full sm:w-full sm:h-3/4 rounded-lg"
+							/>
+						</div>
+						<div className="flex flex-col w-full sm:w-1/2 gap-2">
+							<div className="flex flex-col justify-center gap-1 items-start flex-1 px-3">
+								<h3 className="text-lg px-1">
+									<span className="text-lg font-bold">Title: </span>
+									{show.name}
+								</h3>
+								<p className="line-clamp-4 sm:text-lg px-1">
+									<span className="text-lg font-bold">Details: </span>
+									{show.overview}
+								</p>
+								<div className="flex">
+									<p className="text-lg px-1 w-full">
+										<span className="text-lg font-bold">Rating: </span>
+									</p>
+									<Rating>{starComponents}</Rating>
+								</div>
+								<p className="text-lg px-1">
+									<span className="text-lg font-bold">First Air Date: </span>
+									{show.first_air_date}
+								</p>
+								<div className="flex justify-center gap-4 items-center">
+									<ShowModal show={show} />
+                  <FaThumbsUp className="text-2xl text-blue-500" />
+								</div>
+							</div>
+							<div className="flex flex-col justify-center gap-1 items-start flex-1 px-3">
+								<div className="flex flex-col gap-1 w-full">
+									<form className="w-full">
+										<label htmlFor="review">Review:</label>
+										<textarea
+											name="review"
+											id="review"
+											placeholder="Write a review..."
+											className="w-full h-40 p-2 rounded-lg shadow-md"
+										></textarea>
+										<button
+											type="submit"
+											className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+										>
+											Submit
+										</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+			<hr className="border m-4" />
+			<div className="shadow-md p-2">
+				<ShowCredit />
+			</div>
+		</div>
+	);
 };
 
 export default ShowDetails;
