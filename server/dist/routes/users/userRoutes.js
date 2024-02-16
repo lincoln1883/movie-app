91fe9ba9ca9debb480d9e87f817152b9ce03a987
Lincoln Gibson <lincolngibson7@gmail.com>
2024-02-16 16:21:10 -0300

    Add put,delete, and edit routes for the user.

--- server/dist/routes/users/userRoutes.js
+++ server/dist/routes/users/userRoutes.js
@@ -3,8 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.signUp = void 0;
+exports.readUsers = exports.readUser = exports.removeUser = exports.signUp = exports.updateUser = void 0;
 const express_1 = __importDefault(require("express"));
-const userController_1 = __importDefault(require("../../controllers/users/userController"));
+const userController_1 = require("../../controllers/users/userController");
+const passport_1 = __importDefault(require("../../services/passport"));
 const router = express_1.default.Router();
-exports.signUp = router.post("/signup", userController_1.default);
+exports.updateUser = router.put("/users/:_id", passport_1.default.authenticate("jwt", { session: false }), userController_1.editUser);
+exports.signUp = router.post("/signup", userController_1.createUser);
+exports.removeUser = router.delete("/users/:_id", passport_1.default.authenticate("jwt", { session: false }), userController_1.deleteUser);
+exports.readUser = router.get("/users/:_id", passport_1.default.authenticate("jwt", { session: false }), userController_1.getUser);
+exports.readUsers = router.get("/users", passport_1.default.authenticate("jwt", { session: false }), userController_1.getUsers);
--- server/src/routes/users/userRoutes.ts
+++ server/src/routes/users/userRoutes.ts
@@ -1,6 +1,33 @@
 import express from "express";
-import createUser from "../../controllers/users/userController";
+import {
+	createUser,
+	editUser,
+	deleteUser,
+	getUsers,
+	getUser,
+} from "../../controllers/users/userController";
+import passport from "../../services/passport";
 
 const router = express.Router();
 
+export const updateUser = router.put(
+	"/users/:_id",
+	passport.authenticate("jwt", { session: false }),
+	editUser
+);
 export const signUp = router.post("/signup", createUser);
+export const removeUser = router.delete(
+	"/users/:_id",
+	passport.authenticate("jwt", { session: false }),
+	deleteUser
+);
+export const readUser = router.get(
+	"/users/:_id",
+	passport.authenticate("jwt", { session: false }),
+	getUser
+);
+export const readUsers = router.get(
+	"/users",
+	passport.authenticate("jwt", { session: false }),
+	getUsers
+);
