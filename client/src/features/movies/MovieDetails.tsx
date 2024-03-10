07d42f06802e59d5b02e4ca020e8a020bad40249
Lincoln Gibson <lincolngibson7@gmail.com>
2024-03-10 14:45:29 -0300

    Add api query to get user location to provide accurate info on movie.

--- client/src/features/movies/MovieDetails.tsx
+++ client/src/features/movies/MovieDetails.tsx
@@ -13,13 +13,34 @@ type ProviderType = {
 	provider_name: string;
 	logo_path: string;
 	flatrate: ProviderType[];
+	rent: ProviderType[];
+	buy: ProviderType[];
 };
 
 const MovieDetails = () => {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const [userLocation, setUserLocation] = useState<any>();
 	const [watchProviders, setWatchProviders] = useState<ProviderType>();
 	const { id } = useParams();
 	const dispatch = useAppDispatch();
 	const Navigate = useNavigate();
+
+	useEffect(() => {
+		const getUserLocation = async () => {
+			try {
+				const resp = await fetch(
+					`https://api.geoapify.com/v1/ipinfo?apiKey=${
+						import.meta.env.VITE_APP_GEO_API_KEY
+					}`
+				);
+				const userLocationData = await resp.json();
+				setUserLocation(userLocationData.country.iso_code);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getUserLocation();
+	}, []);
 
 	useEffect(() => {
 		dispatch(fetchMovieById(id as string));
@@ -30,18 +51,20 @@ const MovieDetails = () => {
 
 	useEffect(() => {
 		const getWatchProviders = async () => {
-			const apiKey = import.meta.env.VITE_APP_API_KEY;
-			const response = await fetch(
-				`https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=${apiKey}`
-			);
-			const data = await response.json();
-			console.log(data);
-
-			const watchProviders = data.results.US || data.results.GB || data.results.BR || data.results.JM;
-			setWatchProviders(watchProviders);
+			try {
+				const apiKey = import.meta.env.VITE_APP_API_KEY;
+				const response = await fetch(
+					`https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=${apiKey}`
+				);
+				const data = await response.json();
+				const watchProviders = data.results[userLocation];
+				setWatchProviders(watchProviders);
+			} catch (error) {
+				console.log(error);
+			}
 		};
 		getWatchProviders();
-	}, [movie, id]);
+	}, [id, userLocation]);
 
 	if (!movie) {
 		return <h1>No movie found</h1>;
@@ -80,28 +103,78 @@ const MovieDetails = () => {
 									alt={movie.title}
 									className="w-full h-64 sm:w-full sm:h-72 rounded-t-lg rounded-b-none sm:rounded-none sm:rounded-l-lg flex-1"
 								/>
-								<div>
-									{watchProviders?.flatrate?.map((provider: ProviderType) => (
-										<div
-											key={provider.provider_id}
-											className="flex gap-2 justify-start items-center w-full py-2 px-1"
-										>
-											<h5 className="sm:text-lg px-1">
-												<span className="sm:text-sm font-semibold">
-													Watch Now:{" "}
-												</span>
-											</h5>
-											<img
-												src={`https://image.tmdb.org/t/p/w500${provider.logo_path}`}
-												alt={provider.provider_name}
-												className="w-5 h-5"
-											/>
-											<span className="sm:text-sm dark:text-white">
-												{provider.provider_name}
-											</span>
-										</div>
-									))}
-								</div>
+								{watchProviders && (
+									<div className="flex flex-col w-full gap-1 my-2">
+										{watchProviders?.flatrate?.length > 0 && (
+											<div className="flex justify-start gap-1 items-center flex-wrap">
+												<h5 className="sm:text-lg px-1">
+													<span className="text-sm font-semibold">Watch :</span>
+												</h5>
+												{watchProviders?.flatrate?.map(
+													(provider: ProviderType) => (
+														<div
+															key={provider.provider_id}
+															className="flex gap-1 justify-start items-center px-1"
+														>
+															<img
+																src={`https://image.tmdb.org/t/p/w500${provider.logo_path}`}
+																alt={provider.provider_name}
+																className="w-5 h-5"
+															/>
+															<span className="text-sm dark:text-white">
+																{provider.provider_name.slice(0, 7)}
+															</span>
+														</div>
+													)
+												)}
+											</div>
+										)}
+										{watchProviders?.buy?.length > 0 && (
+											<div className="flex justify-start gap-1 items-center flex-wrap">
+												<h5 className="sm:text-lg px-1">
+													<span className="text-xs font-semibold">Buy :</span>
+												</h5>
+												{watchProviders?.buy?.map((provider: ProviderType) => (
+													<div
+														key={provider.provider_id}
+														className="flex gap-1 justify-start items-center px-1"
+													>
+														<img
+															src={`https://image.tmdb.org/t/p/w500${provider.logo_path}`}
+															alt={provider.provider_name}
+															className="w-5 h-5"
+														/>
+														<span className="text-sm dark:text-white">
+															{provider.provider_name.slice(0, 7)}
+														</span>
+													</div>
+												))}
+											</div>
+										)}
+										{watchProviders?.rent?.length > 0 && (
+											<div className="flex justify-start gap-1  items-center flex-wrap">
+												<h5 className="sm:text-lg px-1">
+													<span className="text-sm font-semibold">Rent :</span>
+												</h5>
+												{watchProviders?.rent.map((provider: ProviderType) => (
+													<div
+														key={provider.provider_id}
+														className="flex gap-1 justify-start items-center px-1"
+													>
+														<img
+															src={`https://image.tmdb.org/t/p/w500${provider.logo_path}`}
+															alt={provider.provider_name}
+															className="w-5 h-5"
+														/>
+														<span className="text-sm dark:text-white">
+															{provider.provider_name.slice(0, 7)}
+														</span>
+													</div>
+												))}
+											</div>
+										)}
+									</div>
+								)}
 							</div>
 							<div className="flex flex-col w-full sm:w-1/2 gap-2">
 								<div className="flex flex-col justify-center gap-1 items-start flex-1">
